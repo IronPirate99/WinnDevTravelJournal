aa8b0b5a5f25b8f6e8a5596375df64653c4cbf3b
MoraswiChuene03 <moraswit200313@gmail.com>
2025-11-25 22:47:25 +0200

    modified dao and util files.

--- src/main/java/.comTravellingJournal/dao/EntryDao.java
+++ src/main/java/.comTravellingJournal/dao/EntryDao.java
@@ -1,3 +1,56 @@
-public class EntryDao {
+package com.travelingjournal.dao;
+
+import com.mongodb.client.MongoCollection;
+import com.travelingjournal.model.JournalEntry;
+import com.travelingjournal.util.DatabaseUtil;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mongodb.client.model.Filters.eq;
+
+public class EntryDAO {
+
+    private MongoCollection<Document> getEntryCollection() {
+        return DatabaseUtil.getDatabase().getCollection("journal_entries");
+    }
+
+    public void addEntry(JournalEntry entry) {
+        Document doc = new Document("trip_id", new ObjectId(entry.getTripId())) // Link to Trip
+                .append("title", entry.getTitle())
+                .append("content", entry.getContent())
+                .append("entry_date", entry.getEntryDate())
+                .append("photo_path", entry.getPhotoPath());
+
+        getEntryCollection().insertOne(doc);
+    }
+
+    public List<JournalEntry> findAllByTripId(String tripId) {
+        List<JournalEntry> entries = new ArrayList<>();
+        
+        // Find by Trip ObjectId
+        for (Document doc : getEntryCollection().find(eq("trip_id", new ObjectId(tripId)))) {
+            JournalEntry entry = new JournalEntry();
+            entry.setId(doc.getObjectId("_id").toHexString());
+            entry.setTripId(doc.getObjectId("trip_id").toHexString());
+            entry.setTitle(doc.getString("title"));
+            entry.setContent(doc.getString("content"));
+            entry.setEntryDate(doc.getDate("entry_date"));
+            entry.setPhotoPath(doc.getString("photo_path"));
+            entries.add(entry);
+        }
+        return entries;
+    }
     
+     public boolean deleteEntry(String entryId) {
+        try {
+            getEntryCollection().deleteOne(eq("_id", new ObjectId(entryId)));
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
--- src/main/java/.comTravellingJournal/dao/UserDao.java
+++ src/main/java/.comTravellingJournal/dao/UserDao.java
@@ -1,3 +1,47 @@
-public class UserDao {
-    
+package com.travelingjournal.dao;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.travelingjournal.model.User;
+import com.travelingjournal.util.DatabaseUtil;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import static com.mongodb.client.model.Filters.eq;
+
+public class UserDAO {
+
+    private MongoCollection<Document> getUserCollection() {
+        return DatabaseUtil.getDatabase().getCollection("users");
+    }
+
+    public boolean registerUser(User user) {
+        Document doc = new Document("username", user.getUsername())
+                .append("email", user.getEmail())
+                .append("password", user.getPassword()); // Remember: Hash this first!
+
+        try {
+            getUserCollection().insertOne(doc);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public User findByEmail(String email) {
+        // "eq" is a filter helper: WHERE email = email
+        Document doc = getUserCollection().find(eq("email", email)).first();
+
+        if (doc != null) {
+            User user = new User();
+            // Retrieve the ObjectId and convert to Hex String for the POJO
+            user.setId(doc.getObjectId("_id").toHexString()); 
+            user.setUsername(doc.getString("username"));
+            user.setEmail(doc.getString("email"));
+            user.setPassword(doc.getString("password"));
+            return user;
+        }
+        return null;
+    }
 }
